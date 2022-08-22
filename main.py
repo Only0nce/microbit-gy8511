290c4cdf9e6760ade44c82e02964879b552533a6
Only0nce <69136923+Only0nce@users.noreply.github.com>
2022-08-22 14:33:20 +0700

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-def ReadLight():
+def Wind():
     while(True):
-        numberOfReadings = 8
-        runningUV = 0
-        runningRef = 0
-        x = 0
-        y = 0
-        for x in range(numberOfReadings):
-            runningUV += pins.analog_read_pin(AnalogPin.P0)
-            runningUV /= numberOfReadings
-            serial.write_number(runningUV)
-        uvLevel = runningUV
-        for y in range(numberOfReadings):
-                runningRef += pins.analog_read_pin(AnalogPin.P1)
-                runningRef /= numberOfReadings
-                serial.write_number(runningRef)
-        refLevel = runningRef
+        sensorValue = pins.analog_read_pin(AnalogPin.P0)
+        outvoltage = sensorValue * (5.0 / 1023.0)
+        Level = 6 * outvoltage
+        basic.show_string("wind")
+        basic.show_number(Level)
+        control.wait_micros(5)
 
-        outputVoltage = 3.3 / refLevel * uvLevel
-        uvIntensity = Math.map(outputVoltage, 0.99, 2.8, 0.0, 15.0)
-        # serial.write_line("output: ")
-        # serial.write_number(refLevel)
-        # serial.write_line("ML8511 output: ")
-        # serial.write_number(uvLevel)
-        # serial.write_line(" / ML8511 voltage: ")
-        # serial.write_number(outputVoltage)
-        # serial.write_line(" / UV Intensity (mW/cm^2): ")
-        # serial.write_number(uvIntensity)
-        # basic.show_string("O: ")
-        # basic.show_number(refLevel)
-        # basic.show_string("MLO: ")
-        # basic.show_number(uvLevel)
-        # basic.show_string("MLV: ")
-        # basic.show_number(outputVoltage)
-        basic.show_string("UV: ")
-        basic.show_number(uvIntensity)
-        control.wait_micros(50)
-
-ReadLight()
+Wind()
